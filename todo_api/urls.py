7455d60ea0077377e5fbf1640cc8a62c0341e83d
muhammadezee343 <muhammade.zee343@gmail.com>
2025-05-10 00:15:55 +0500

    create and get todos apis

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import TodoListApiView
 
-urlpatterns = []
+urlpatterns = [
+    path("api", TodoListApiView.as_view())
+]
--- todo_api/views.py
+++ todo_api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+class TodoListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        todos = Todo.objects.filter(user = request.user.id)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        data={
+            'task': request.data.get('task'),
+            'complated': request.data.get('complated'),
+            'user': request.user.id
+        }
+
+        serializer = TodoSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
